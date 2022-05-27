0e626c0ef4e2b5b2ecce2f630529c5a245d0b28a
Alex_x90 <sashanrosenthal@gmail.com>
2022-05-26 18:00:27 -0700

    Set up loading .my.cnf to configure DB.

--- db-connector.js
+++ db-connector.js
@@ -1,13 +1,18 @@
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
+var ini = require('node-ini');
+
+var config = ini.parseSync('../../.my.cnf');
+
+console.log(config);
 
 // Create a 'connection pool' using the provided credentials
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_[your_onid]',
-    password        : '[your_db_password]',
-    database        : 'cs340_[your_onid]'
+    host            : config.host,
+    user            : config.user,
+    password        : config.password,
+    database        : config.database
 })
 
 // Export it for use in our application
